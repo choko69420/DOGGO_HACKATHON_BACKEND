a9b8c16a255947c26c9a70dcd46abffc2f9f79ab
choko69420 <hack3r69420@gmail.com>
2022-11-20 14:50:16 +0400

    bugfix

--- DOGGO_backend/settings.py
+++ DOGGO_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ymg+#%txv_-27(vnd@r#pbzk#qmvgk@d(#c)&8v_hy%!%(#sgh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['choko69.pythonanywhere.com', ]
 
 
 # Application definition
